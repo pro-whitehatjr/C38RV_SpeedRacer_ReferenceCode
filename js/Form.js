9f928eb222eeab31f41bfb1d1fd296b18f4f53e7
whitehatjr <rajeevwhitehatjr@gmail.com>
2021-08-02 13:22:20 +0530

    Css Changed

--- js/Form.js
+++ js/Form.js
@@ -7,7 +7,7 @@ class Form {
   }
 
   setElementsPosition() {
-    this.titleImg.position(120, 100);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
